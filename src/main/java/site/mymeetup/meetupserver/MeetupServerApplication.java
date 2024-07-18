2e1a99eaaa4366fff7fb14834f4da0fac0a0a6fd
HS <lhs991117@gmail.com>
2024-07-18 10:22:29 +0900

    first commit

---
+++ src/main/java/site/mymeetup/meetupserver/MeetupServerApplication.java
@@ -0,0 +1,13 @@
+package site.mymeetup.meetupserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetupServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetupServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/site/mymeetup/meetupserver/MeetupServerApplicationTests.java
@@ -0,0 +1,13 @@
+package site.mymeetup.meetupserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetupServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
