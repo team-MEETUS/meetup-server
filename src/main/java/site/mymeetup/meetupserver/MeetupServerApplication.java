dc34d48dd587016c9489cafeb89557a56e36129b
HS <lhs991117@gmail.com>
2024-07-25 15:07:22 +0900

    test: 배포 테스트

--- src/main/java/site/mymeetup/meetupserver/MeetupServerApplication.java
+++ src/main/java/site/mymeetup/meetupserver/MeetupServerApplication.java
@@ -2,8 +2,9 @@ package site.mymeetup.meetupserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class MeetupServerApplication {
 
     public static void main(String[] args) {
