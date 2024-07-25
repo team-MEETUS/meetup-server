61ec395c76732e57f4b168f857cc60233cc10488
HS <lhs991117@gmail.com>
2024-07-25 16:48:10 +0900

    test: 배포 테스트

--- src/test/java/site/mymeetup/meetupserver/MeetupServerApplicationTests.java
+++ src/test/java/site/mymeetup/meetupserver/MeetupServerApplicationTests.java
@@ -3,7 +3,7 @@ package site.mymeetup.meetupserver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = MeetupServerApplicationTests.class)
 class MeetupServerApplicationTests {
 
     @Test
