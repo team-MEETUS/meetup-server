7bbf4054ff81b176886e09df1554fd3c3599ce3a
YSL0711 <dldustj1234@gmail.com>
2024-08-06 15:36:58 +0900

    feat/member/#44 멤버 서비스 부분 수정

--- src/main/java/site/mymeetup/meetupserver/member/service/MemberService.java
+++ src/main/java/site/mymeetup/meetupserver/member/service/MemberService.java
@@ -1,8 +1,10 @@
 package site.mymeetup.meetupserver.member.service;
 
+
 import site.mymeetup.meetupserver.member.dto.MemberDto;
 
 public interface MemberService {
     MemberDto.MemberSaveRespDto createMember(MemberDto.MemberSaveReqDto memberSaveReqDto);
+
     MemberDto.MemberSelectRespDto getMemberByMemberId(Long memberId);
 }
