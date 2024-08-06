b5162dc8ebbeb45d9d7d6e5b5e40e85a3e04ac8c
YSL0711 <dldustj1234@gmail.com>
2024-08-06 12:48:42 +0900

    refactor:회원관심사 삭제

--- src/main/java/site/mymeetup/meetupserver/memberAndInterest/service/MemberAndInterestService.java
+++
@@ -1,8 +0,0 @@
-package site.mymeetup.meetupserver.memberAndInterest.service;
-
-import java.util.List;
-import site.mymeetup.meetupserver.memberAndInterest.dto.MemberAndInterestDto;
-
-public interface MemberAndInterestService {
-    List<MemberAndInterestDto.MemberAndInterestRespDto> getAllMemberAndInterest();
-}
