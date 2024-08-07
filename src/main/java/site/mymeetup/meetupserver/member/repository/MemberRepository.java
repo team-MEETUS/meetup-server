5432b73acad877da6d52084f12714f2a06144e0f
YSL0711 <dldustj1234@gmail.com>
2024-08-07 11:35:29 +0900

    refactor: 멤버 리팩터링

--- src/main/java/site/mymeetup/meetupserver/member/repository/MemberRepository.java
+++ src/main/java/site/mymeetup/meetupserver/member/repository/MemberRepository.java
@@ -1,16 +1,16 @@
 package site.mymeetup.meetupserver.member.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+
 import site.mymeetup.meetupserver.member.entity.Member;
 
+import java.util.List;
 import java.util.Optional;
 
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    // id와 상태값으로 멤버 찾기
-    Optional<Member> findByMemberIdAndStatus(Long memberId, Integer status);
+    // id와 상태값으로 멤버 조회
+    Optional<Member> findByMemberIdAndStatus(Long memberId, int status);
 
 }
