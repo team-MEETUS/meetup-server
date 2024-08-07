c202955a33244eaf63d2fe84e2f2f18c142b24d7
HS <lhs991117@gmail.com>
2024-08-07 12:28:26 +0900

    feat: 게시글 에러코드 추가

--- src/main/java/site/mymeetup/meetupserver/exception/ErrorCode.java
+++ src/main/java/site/mymeetup/meetupserver/exception/ErrorCode.java
@@ -33,6 +33,7 @@ public enum ErrorCode {
     BOARD_CREW_ACCESS_DENIED(HttpStatus.FORBIDDEN, "B40303", "접근할 수 없는 게시글입니다."),
     BOARD_NOT_FOUND(HttpStatus.NOT_FOUND, "B40401", "존재하지 않는 게시글입니다."),
     BOARD_CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "B40402", "존재하지 않는 카테고리입니다."),
+    BOARD_DELETE_ACCESS_DENIED(HttpStatus.FORBIDDEN, "B40304", "삭제는 작성자 또는 운영진 모임장만 가능합니다."),
 
     // 관심사
     INTEREST_BAD_REQUEST(HttpStatus.BAD_REQUEST, "I40001", "관심사 대분류에 속한 소분류가 아닙니다."),
