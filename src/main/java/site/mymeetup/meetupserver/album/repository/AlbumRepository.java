f003b37d66fd61592ca862176ffd32e9ff7598db
JH <middledown12@gmail.com>
2024-08-08 15:58:54 +0900

    feat: 사진첩 조회

--- src/main/java/site/mymeetup/meetupserver/album/repository/AlbumRepository.java
+++ src/main/java/site/mymeetup/meetupserver/album/repository/AlbumRepository.java
@@ -2,6 +2,8 @@ package site.mymeetup.meetupserver.album.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import site.mymeetup.meetupserver.album.entity.Album;
+import java.util.List;
 
 public interface AlbumRepository extends JpaRepository<Album, Long> {
+    List<Album> findAlbumByCrewCrewId(Long crewId);
 }
--- src/main/java/site/mymeetup/meetupserver/album/service/AlbumService.java
+++ src/main/java/site/mymeetup/meetupserver/album/service/AlbumService.java
@@ -1,13 +1,13 @@
 package site.mymeetup.meetupserver.album.service;
 
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import static site.mymeetup.meetupserver.album.dto.AlbumDto.AlbumRespDto;
 import static site.mymeetup.meetupserver.album.dto.AlbumDto.AlbumSaveRespDto;
-import static site.mymeetup.meetupserver.album.dto.AlbumDto.AlbumSaveReqDto;
-
 import java.util.List;
 
 public interface AlbumService {
 
     List<AlbumSaveRespDto> createAlbum(Long crewId, List<MultipartFile> images);
+
+    List<AlbumRespDto> getAlbumByCrewId(Long crewId);
 }
