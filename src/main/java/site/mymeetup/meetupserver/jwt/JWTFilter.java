61695275fe3f808f0cedf908d7503df413337f11
chaeyoon <chaeyoon010809@gmail.com>
2024-08-12 16:48:37 +0900

    feat: 시큐리티 검증하지 않을 URI 추가

--- src/main/java/site/mymeetup/meetupserver/jwt/JWTFilter.java
+++ src/main/java/site/mymeetup/meetupserver/jwt/JWTFilter.java
@@ -31,13 +31,15 @@ public class JWTFilter extends OncePerRequestFilter {
 
         String requestURI = request.getRequestURI();
         String method = request.getMethod();
-        if (method.equals("GET") && (requestURI.matches("/api/v1/crews/\\d+") ||
-                                     requestURI.equals("/api/v1/crews") ||
-                                     requestURI.matches("/api/v1/crews/\\d+/members") ||
-                                     requestURI.equals("/api/v1/geos") ||
+        if (method.equals("GET") && (requestURI.equals("/api/v1/geos") ||
                                      requestURI.equals("/api/v1/interestBigs") ||
                                      requestURI.matches("/api/v1/interestBigs/\\d+/interestSmalls") ||
                                      requestURI.matches("/api/v1/members/\\d+") ||
+                                     requestURI.equals("/api/v1/crews") ||
+                                     requestURI.matches("/api/v1/crews/\\d+") ||
+                                     requestURI.matches("/api/v1/crews/\\d+/members") ||
+                                     requestURI.matches("/api/v1/crews/\\d+/meetings") ||
+                                     requestURI.matches("/api/v1/crews/\\d+/meetings/\\d+") ||
                                      requestURI.matches("/api/v1/crews/\\d+/albums") ||
                                      requestURI.matches("/api/v1/crews/\\d+/boards") ||
                                      requestURI.matches("/api/v1/crews/\\d+/boards/.+"))) {
