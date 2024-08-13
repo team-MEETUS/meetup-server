53f39aa95c18ff3c80b4333e44a53d31f90bbc63
HS <lhs991117@gmail.com>
2024-08-13 16:28:38 +0900

    feat: 게시글 페이지 처리

--- src/main/java/site/mymeetup/meetupserver/board/repository/BoardRepository.java
+++ src/main/java/site/mymeetup/meetupserver/board/repository/BoardRepository.java
@@ -1,5 +1,7 @@
 package site.mymeetup.meetupserver.board.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import site.mymeetup.meetupserver.board.entity.Board;
 
@@ -7,8 +9,8 @@ import java.util.List;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
     // crewId로 게시글 조회
-    List<Board> findBoardByCrew_CrewIdAndStatusNot(Long crewId, int status);
+    Page<Board> findBoardByCrew_CrewIdAndStatusNot(Long crewId, int status, Pageable pageable);
 
     // crewId & category 로 게시글 조회
-    List<Board> findBoardByCrew_CrewIdAndCategoryAndStatusNot(Long crewId, String category, int status);
+    Page<Board> findBoardByCrew_CrewIdAndCategoryAndStatusNot(Long crewId, String category, int status, Pageable pageable);
 }
