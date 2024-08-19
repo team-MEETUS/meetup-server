4f8567d07107d46f5053982a6fb8a4d6428e8a50
JH <middledown12@gmail.com>
2024-08-19 17:52:52 +0900

    refactor: 채팅 리팩토링

--- src/main/java/site/mymeetup/meetupserver/chat/dto/ChatDto.java
+++ src/main/java/site/mymeetup/meetupserver/chat/dto/ChatDto.java
@@ -1,9 +1,6 @@
 package site.mymeetup.meetupserver.chat.dto;
 
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import site.mymeetup.meetupserver.chat.entity.Chat;
 
 import java.time.LocalDateTime;
@@ -12,7 +9,6 @@ import java.util.UUID;
 public class ChatDto {
 
     @Getter
-
     @NoArgsConstructor(access = AccessLevel.PROTECTED)
     public static class ChatRespDto {
         private String id;
@@ -30,6 +26,23 @@ public class ChatDto {
             this.receiverId = chat.getReceiverId();
             this.crewId = chat.getCrewId();
             this.createDate = chat.getCreateDate();
+        }
+    }
+
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class ChatSaveReqDto {
+        private String id;
+        private Long senderId;
+        private Long receiverId;
+        private String message;
+
+        @Builder
+        public ChatSaveReqDto(Chat chat) {
+            this.id = UUID.randomUUID().toString();
+            this.message = chat.getMessage();
+            this.senderId = chat.getSenderId();
+            this.receiverId = chat.getReceiverId();
         }
     }
 }
--- src/main/java/site/mymeetup/meetupserver/chat/service/ChatServiceImpl.java
+++ src/main/java/site/mymeetup/meetupserver/chat/service/ChatServiceImpl.java
@@ -7,18 +7,23 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import static site.mymeetup.meetupserver.chat.dto.ChatDto.ChatRespDto;
 
+import site.mymeetup.meetupserver.chat.dto.ChatDto;
 import site.mymeetup.meetupserver.chat.entity.Chat;
 import site.mymeetup.meetupserver.chat.repository.ChatRepository;
-//import site.mymeetup.meetupserver.crew.repository.CrewRepository;
-//import site.mymeetup.meetupserver.exception.CustomException;
-//import site.mymeetup.meetupserver.exception.ErrorCode;
+import site.mymeetup.meetupserver.crew.entity.Crew;
 import site.mymeetup.meetupserver.crew.entity.CrewMember;
 import site.mymeetup.meetupserver.crew.repository.CrewMemberRepository;
+import site.mymeetup.meetupserver.crew.repository.CrewRepository;
+import site.mymeetup.meetupserver.crew.role.CrewMemberRole;
 import site.mymeetup.meetupserver.exception.CustomException;
 import site.mymeetup.meetupserver.exception.ErrorCode;
+import site.mymeetup.meetupserver.member.entity.Member;
+import site.mymeetup.meetupserver.member.repository.MemberRepository;
 import site.mymeetup.meetupserver.response.ApiResponse;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -27,21 +32,22 @@ public class ChatServiceImpl implements ChatService {
     private final ChatRepository chatRepository;
     private final SimpMessagingTemplate messagingTemplate;
     private final CrewMemberRepository crewMemberRepository;
+    private final CrewRepository crewRepository;
+    private final MemberRepository memberRepository;
 
     @Override
-    public Mono<ApiResponse<ChatRespDto>> createChat(ChatRespDto chatRespDto) {
-
-        if (chatRespDto.getSenderId() == null || chatRespDto.getMessage() == null) {
+    public Mono<ApiResponse<ChatRespDto>> createChat(Long crewId, ChatDto.ChatSaveReqDto chatSaveReqDto, Long senderId) {
+        if (senderId == null || chatSaveReqDto.getMessage() == null) {
             return Mono.just(ApiResponse.error(ErrorCode.CHAT_NOT_FOUND));
         }
 
         Chat chat = Chat.builder()
                 .id(UUID.randomUUID().toString())
-                .message(chatRespDto.getMessage())
-                .senderId(chatRespDto.getSenderId())
-                .receiverId(chatRespDto.getReceiverId())
+                .message(chatSaveReqDto.getMessage())
+                .senderId(senderId)
+                .receiverId(chatSaveReqDto.getReceiverId())
                 .createDate(LocalDateTime.now())
-                .crewId(chatRespDto.getCrewId())
+                .crewId(crewId)
                 .build();
 
         return chatRepository.save(chat)
@@ -49,10 +55,24 @@ public class ChatServiceImpl implements ChatService {
                 .map(savedChat -> ApiResponse.success(ChatRespDto.builder().chat(chat).build()));
     }
 
-
     @Override
     public Flux<ApiResponse<ChatRespDto>> getAllChatByCrewId(Long crewId, Long senderId) {
-        CrewMember crewMember = crewMemberRepository.findByCrew_CrewIdAndMember_MemberId(crewId, senderId).orElseThrow(() -> new CustomException(ErrorCode.CREW_MEMBER_NOT_FOUND));
+        // 해당 유저가 존재하는지 검증
+        Member member = memberRepository.findByMemberIdAndStatus(senderId, 1)
+                .orElseThrow(() -> new CustomException(ErrorCode.MEMBER_NOT_FOUND));
+        // 해당 모임이 존재하는지 검증
+        Crew crew = crewRepository.findByCrewIdAndStatus(crewId, 1)
+                .orElseThrow(() -> new CustomException(ErrorCode.CREW_NOT_FOUND));
+        // 해당 유저가 모임원인지 검증
+        List<CrewMemberRole> roles = Arrays.asList(
+                CrewMemberRole.MEMBER,
+                CrewMemberRole.ADMIN,
+                CrewMemberRole.LEADER
+        );
+
+        CrewMember crewMember = crewMemberRepository.findByCrewAndMemberAndRoleIn(crew, member, roles)
+                .orElseThrow(() -> new CustomException(ErrorCode.CREW_MEMBER_NOT_FOUND));
+
         return chatRepository.findAllByCrewIdAndCreateDateAfter(crewId, crewMember.getCreateDate())
                 .map(chat -> {
                     ChatRespDto chatRespDto = ChatRespDto.builder().chat(chat).build();
